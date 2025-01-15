4c7848e7f9be587c384963b520969fee225b176a
Ágata Brazão <106405408+AgataCli@users.noreply.github.com>
2025-01-14 23:35:05 -0400

    refactor: retornando a raw no monitor serial
    Saída: RECV_freq,raw[0],raw[1],...,raw[n]

--- prototipo/receiver.ino
+++ prototipo/receiver.ino
@@ -1,19 +1,30 @@
 #include <IRremote.h>
-#define PIN_RECEIVER 2   // Define o pino onde o receptor infravermelho está conectado
+#define PIN_RECEIVER 2
 
-IRrecv receiver(PIN_RECEIVER); // Cria um objeto IRrecv associado ao pino do receptor
+IRrecv receiver(PIN_RECEIVER);
 
-void setup()
-{
-  Serial.begin(115200); // Inicializa a comunicação serial a 115200 bauds
-  Serial.println("<Pressione um botão>"); // Exibe uma mensagem no monitor serial
-  receiver.enableIRIn(); // Inicializa o receptor infravermelho para receber sinais infravermelhos
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Aguardando sinal IR...");
+  receiver.enableIRIn();
 }
 
-void loop()
-{
-  if (receiver.decode()) { // Verifica se um sinal infravermelho foi decodificado
-    Serial.print(receiver.decodedIRData.command);// Exibe o código do comando infravermelho
-    receiver.resume(); // Reinicia o receptor infravermelho para receber mais comandos
+void loop() {
+  if (receiver.decode()) {
+    //Serial.print(receiver.decodedIRData.command);
+    Serial.print("RECV_freq");
+    //Serial.print(receiver.decodedIRData.frequency);
+    Serial.print(",");
+
+    // Exibe os valores raw como uma lista separada por vírgulas
+    for (uint16_t i = 1; i < receiver.decodedIRData.rawDataPtr->rawlen; i++) {
+      Serial.print(receiver.decodedIRData.rawDataPtr->rawbuf[i] * USECPERTICK);
+      if (i < receiver.decodedIRData.rawDataPtr->rawlen - 1) {
+        Serial.print(","); // Adiciona vírgula se não for o último valor
+      }
+    }
+
+    Serial.println();
+    receiver.resume(); // Pronto para receber outro sinal
   }
 }
