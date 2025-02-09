273d82888de362bdc37371f571a304014c31b1d3
Bianka <bianka.vm03@gmail.com>
2025-02-09 16:29:46 -0400

    chore: remove unnecessary coments

--- smartinfrared-kernel-module/script.sh
+++ smartinfrared-kernel-module/script.sh
@@ -2,7 +2,6 @@
 
 module="smartinfrared.ko"
 
-# Function to remove the kernel module
 remove_module() {
     echo "Removing kernel module..."
     sudo rmmod $module 2>/dev/null || echo "Module not loaded."
@@ -10,10 +9,8 @@ remove_module() {
     make clean
 }
 
-# Variable to hold the background process ID
 dmesg_pid=0
 
-# Function to stop the background process of dmesg
 stop_dmesg() {
     if [[ $dmesg_pid -ne 0 ]]; then
         echo "Parando monitoramento de logs do SmartInfrared (PID: $dmesg_pid)..."
@@ -23,11 +20,10 @@ stop_dmesg() {
     fi
 }
 
-# Trap Ctrl+C to call cleanup functions
 trap "remove_module; stop_dmesg; exit 0" SIGINT
 
 while true; do
-    # Display menu
+
     echo "Select an option:"
     echo "1 - Iniciar os processos"
     echo "2 - Executar lsusb -t"
@@ -39,43 +35,36 @@ while true; do
 
     case $option in
         1)
-            # Inicia os processos
             make
 
             echo "Removing kernel module: cp210x"
-            # Remove the Arduino module with sudo, ignoring errors if not loaded
             sudo rmmod cp210x 2>/dev/null || echo "Module cp210x not loaded."
 
             echo "Installing kernel module: {$module}" 
-            # Insert the kernel module with sudo
             time sudo insmod $module
             ;;
 
         2)
-            # Executa o comando lsusb -t
             echo "Executing lsusb -t"
             lsusb -t
             ;;
 
         3)
-            # Remove o módulo chamando a função
             remove_module
             stop_dmesg
             ;;
 
         4)
-            # Executa o comando para monitorar logs do SmartInfrared em segundo plano
             if [[ $dmesg_pid -eq 0 ]]; then
                 echo "Monitorando logs do SmartInfrared em segundo plano..."
                 sudo dmesg -w &
-                dmesg_pid=$!  # Armazena o PID do processo de segundo plano
+                dmesg_pid=$!
             else
                 echo "Monitoramento de logs já está em execução."
             fi
             ;;
 
         5)
-            # Para o monitoramento de logs e remove o módulo antes de sair
             remove_module
             stop_dmesg
             echo "Saindo..."
@@ -88,6 +77,5 @@ while true; do
             ;;
     esac
 
-    # Pausa para aguardar uma nova entrada
     echo ""
 done
